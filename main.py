b7527609e2065e2a9aac536dfa930aa9289043d8
William O'Neill <99112665+OneillWillam17@users.noreply.github.com>
2022-03-25 23:12:17 -0400

    Update main.py
    adjusted readability of main.py

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ ethereal.ethereal_scrape()
 
 # checks if teams that played are in master_teams list and scores are available
 for match in scrape.info:
+    
     if match['Home team'] in master_teams and match['score'] != 'Scores not submitted yet':
+        
         master_matches.append(match)
 
 
 def match_info():
     """reformats info inside master matches, outputs as plain text with w/l"""
     for game in master_matches:
+        
         home = game['Home team']
         score = game['score']
         away = game['Away team']
@@ -31,12 +34,15 @@ def match_info():
         # checks if home team won or lost
         score1_int = int(score[0:2])
         score2_int = int(score[5:7])
+        
         if score1_int > score2_int:
             winloss = 'wins'
         else:
             winloss = "loses"
+            
         global plain_txt_match
         plain_txt_match.append(f'{home} {winloss} vs {away} ({score})')
+        
         print(plain_txt_match)
 
 
@@ -59,12 +65,14 @@ async def on_message(message):
 
     if message.content == '!scores':
         await message.channel.send("Recent master matches: \n")
+        
         for i in range(len(plain_txt_match)):
             await message.channel.send(plain_txt_match[i])
 
     if message.content == '!ethereal':
-        await message.channel.send('WIP, probably missing some matches LMAO')
+        await message.channel.send('WIP, probably missing some matches')
         for each in ethereal.info:
+            
             eth_home = each['Home team']
             eth_score = each['score']
             eth_away = each['Away team']
@@ -72,10 +80,12 @@ async def on_message(message):
             # checks if home team won or lost
             score1_int = int(eth_score[0:2])
             score2_int = int(eth_score[5:7])
+            
             if score1_int > score2_int:
                 winloss = 'wins'
             else:
                 winloss = "loses"
+                
             await message.channel.send(f'{eth_home} {winloss} vs {eth_away} ({eth_score})')
 
 
